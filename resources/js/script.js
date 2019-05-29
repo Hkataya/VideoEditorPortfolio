690b26b464f34bfd52c5925fb5881da44b6989e8
Hkataya <Hkataya@users.noreply.github.com>
2019-05-29 04:06:40 +0300

    deleted jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,19 @@
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
+    
 }   
+
+window.onload = function(){
+    
+  document.getElementById("header").style.display="block";
+  document.getElementById("desc").style.display="block";
+  document.getElementById("headnav").style.display="block";
+  document.getElementById("header").classList.add("headerClass");
+  document.getElementById("desc").classList.add("descClass");
+  document.getElementById("headnav").classList.add("headnavClass");
+    
+}
+
 
 
 document.getElementsByClassName("btn")[0].onclick = function(){
